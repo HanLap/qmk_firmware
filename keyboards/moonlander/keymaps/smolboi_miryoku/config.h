4118d169f9338142bdb4520bd7d3f54190fba644
Hannah Lappe <hannah@lappe.page>
2023-08-05 12:20:19 +0200

    added miryoku layout

--- keyboards/moonlander/keymaps/smolboi_miryoku/config.h
+++ keyboards/moonlander/keymaps/smolboi_miryoku/config.h
@@ -20,18 +20,17 @@
 
 #define XXX KC_NO
 
-#define LAYOUT_smolboi(\
-  K40, K00, K01, K02, K03, K04,               K05, K06, K07, K08, K09, K43,\
-  K41, K10, K11, K12, K13, K14,               K15, K16, K17, K18, K19, K44,\
-  K42, K20, K21, K22, K23, K24,               K25, K26, K27, K28, K29, K45,\
-                           K38,               K39,\
-                 K32, K33, K34,               K35, K36, K37\
+#define LAYOUT_miryoku(\
+     K00, K01, K02, K03, K04,               K05, K06, K07, K08, K09,\
+     K10, K11, K12, K13, K14,               K15, K16, K17, K18, K19,\
+     K20, K21, K22, K23, K24,               K25, K26, K27, K28, K29,\
+               K32, K33, K34,               K35, K36, K37\
 )\
 LAYOUT_moonlander(\
 XXX, XXX, XXX, XXX, XXX, XXX, XXX,     XXX, XXX, XXX, XXX, XXX, XXX, XXX,\
-K40, K00, K01, K02, K03, K04, XXX,     XXX, K05, K06, K07, K08, K09, K43,\
-K41, K10, K11, K12, K13, K14, XXX,     XXX, K15, K16, K17, K18, K19, K44,\
-K42, K20, K21, K22, K23, K24,               K25, K26, K27, K28, K29, K45,\
-XXX, XXX, XXX, XXX, K32,      K38,     K39,      K37, XXX, XXX, XXX, XXX,\
+XXX, K00, K01, K02, K03, K04, XXX,     XXX, K05, K06, K07, K08, K09, XXX,\
+XXX, K10, K11, K12, K13, K14, XXX,     XXX, K15, K16, K17, K18, K19, XXX,\
+XXX, K20, K21, K22, K23, K24,               K25, K26, K27, K28, K29, XXX,\
+XXX, XXX, XXX, XXX, K32,      XXX,     XXX,      K37, XXX, XXX, XXX, XXX,\
                     K33, K34, XXX,     XXX, K35, K36\
 )
--- keyboards/moonlander/keymaps/smolboi_miryoku/keymap.c
+++ keyboards/moonlander/keymaps/smolboi_miryoku/keymap.c
@@ -20,6 +20,16 @@
 
 #define MY_ESC MT(MOD_LCTL, KC_ESCAPE)
 
+#define TG_MEDI LT(MEDI, KC_ESC )
+#define TG_NAV  LT(NAV , KC_SPC )
+#define TG_MOUS LT(MOUS, KC_TAB )
+
+#define TG_SYMB LT(SYMB, KC_ENT )
+#define TG_NUM  LT(NUM , KC_BSPC)
+#define TG_FUN  LT(FUN , KC_DEL )
+
+#define TG_BUTN LT(BUTN, KC_Z   )
+
 
 enum custom_keycodes {
   RGB_SLD = SAFE_RANGE,
@@ -28,42 +38,66 @@ enum custom_keycodes {
 
 enum layouts {
   BASE,
+  NAV ,
+  MOUS,
+  BUTN,
+  MEDI,
+  NUM ,
   SYMB,
-  SYM2,
-  MEDI
+  FUN ,
 };
 
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [BASE] = LAYOUT_smolboi(
-    KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,        KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,      
-    KC_HOME, MY_AGUI, MY_SALT, MY_DSFT, MY_FCTL, KC_G   ,        KC_H   , MY_JCTL, MY_KSFT, MY_LALT, MY_CGUI, KC_QUOT,       
-    CW_TOGG, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,        KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,      
-                                                 KC_LSFT,        MO(3)  ,                       
-                               MO(1)  , KC_SPC , KC_ESC ,        KC_ENT , KC_BSPC, MO(2) 
+  [BASE] = LAYOUT_miryoku(
+    KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,        KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   ,      
+    MY_AGUI, MY_SALT, MY_DSFT, MY_FCTL, KC_G   ,        KC_H   , MY_JCTL, MY_KSFT, MY_LALT, MY_CGUI,       
+    TG_BUTN, KC_X   , KC_C   , KC_V   , KC_B   ,        KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH,      
+                      TG_MEDI, TG_NAV , TG_MOUS,        TG_SYMB, TG_NUM , TG_FUN 
   ),
-  [SYMB] = LAYOUT_smolboi(
-    _______, KC_EXLM, KC_AT  , KC_LCBR, KC_RCBR, KC_PIPE,        KC_EQL , KC_7   , KC_8   , KC_9   , KC_ASTR, _______,         
-    _______, KC_HASH, KC_DLR , KC_LPRN, KC_RPRN, KC_GRV ,        KC_MINS, KC_4   , KC_5   , KC_6   , KC_PLUS, _______,    
-    _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,        KC_UNDS, KC_1   , KC_2   , KC_3   , KC_AMPR, _______, 
-                                                 _______,        _______,     
-                               _______, _______, _______,        KC_RALT, _______, KC_0 
+  [NAV ] = LAYOUT_miryoku(
+    _______, _______, _______, _______, _______,        KC_CUT , KC_COPY, KC_PSTE, KC_UNDO, _______,      
+    KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, _______,        KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______,       
+    _______, _______, _______, _______, _______,        KC_HOME, KC_PGDN, KC_PGUP, KC_END , KC_INS ,      
+                      _______, _______, _______,        KC_ENT , KC_BSPC, KC_DEL 
   ),
-  [SYM2] = LAYOUT_smolboi(
-    _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,        KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11,  
-    _______, KC_TAB , _______, _______, _______, KC_DEL ,        KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, KC_F12,
-    _______, _______, _______, _______, _______, _______,        KC_HOME, KC_PGDN, KC_PGUP, KC_END , _______, _______, 
-                                                 _______,        _______,                
-                               _______, _______, _______,        _______, _______, _______
+  [MOUS] = LAYOUT_miryoku(
+    _______, _______, _______, _______, _______,        KC_CUT , KC_COPY, KC_PSTE, KC_UNDO, _______,      
+    KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, _______,        KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______,       
+    _______, _______, _______, _______, _______,        KC_WH_L, KC_WH_D, KC_MS_U, KC_MS_R, KC_INS ,      
+                      _______, _______, _______,        KC_BTN2, KC_BTN1, KC_BTN3 
   ),
-  [MEDI] = LAYOUT_smolboi(
-    _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______,  
-    _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, QK_BOOT, 
-                                                 RGB_TOG,        _______,                
-                               _______, KC_ESC , RGB_MOD,        _______, _______, _______
+  [BUTN] = LAYOUT_miryoku(
+    _______, _______, _______, _______, _______,        _______, _______, _______, _______, QK_BOOT,      
+    KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, _______,        _______, _______, _______, _______, _______,       
+    _______, KC_CUT , KC_COPY, KC_PSTE, _______,        _______, _______, _______, _______, _______,      
+                      _______, _______, _______,        _______, _______, _______ 
   ),
+  [MEDI] = LAYOUT_miryoku(
+    _______, _______, _______, _______, _______,        _______, _______, _______, _______, QK_BOOT,      
+    KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, _______,        _______, _______, _______, _______, _______,       
+    _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______,      
+                      _______, _______, _______,        _______, _______, _______ 
+  ),
+  [NUM ] = LAYOUT_miryoku(
+    KC_LBRC, KC_7   , KC_8   , KC_9   , KC_RBRC,        _______, _______, _______, _______, _______,      
+    KC_QUOT, KC_4   , KC_5   , KC_6   , KC_EQL ,        _______, KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI,       
+    KC_GRV , KC_1   , KC_2   , KC_3   , KC_BSLS,        _______, _______, _______, _______, _______,      
+                      KC_0   , KC_DOT , KC_MINS,        _______, _______, _______ 
+  ),
+  [SYMB] = LAYOUT_miryoku(
+    KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,        _______, _______, _______, _______, _______,      
+    KC_DQT , KC_DLR , KC_PERC, KC_CIRC, KC_PLUS,        _______, KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI,       
+    KC_TILD, KC_EXLM, KC_AT  , KC_HASH, KC_PIPE,        _______, _______, _______, _______, _______,      
+                      KC_LPRN, KC_RPRN, KC_UNDS,        _______, _______, _______ 
+  ),
+  [FUN ] = LAYOUT_miryoku(
+    KC_F12 , KC_F7  , KC_F8  , KC_F9  , KC_PSCR,        _______, _______, _______, _______, _______,      
+    KC_F11 , KC_F4  , KC_F5  , KC_F6  , _______,        _______, KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI,       
+    KC_F10 , KC_F1  , KC_F2  , KC_F3  , _______,        _______, _______, _______, _______, _______,      
+                      KC_ESC , KC_SPC , KC_TAB ,        _______, _______, _______ 
+  )
 };
 
 extern rgb_config_t rgb_matrix_config;
@@ -128,7 +162,7 @@ bool rgb_matrix_indicators_user(void) {
     case SYMB:
       set_layer_color(SYMB);
       break;
-    case SYM2:
+    case NAV:
       rgb_matrix_set_color_all(200, 200,  255);
       break;
     case MEDI:
