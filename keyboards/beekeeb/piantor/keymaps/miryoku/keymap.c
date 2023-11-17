ba48fad66d43281d7cba01971fc72bedc52f317a
Hannah Lappe <hannah@lappe.page>
2023-11-17 13:00:25 +0100

    paintor:miryoku - added boot button

--- keyboards/beekeeb/piantor/keymaps/miryoku/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/miryoku/keymap.c
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       _______, _______, _______,        _______, _______, _______ 
   ),
   [MEDI] = LAYOUT_miryoku(
-    _______, _______, _______, _______, _______,        _______, _______, _______, _______, QK_BOOT,      
+    _______, _______, _______, _______, QK_BOOT,        _______, _______, _______, _______, QK_BOOT,      
     KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, _______,        _______, _______, _______, _______, _______,       
     _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______,      
                       _______, _______, _______,        _______, _______, _______ 
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       KC_LPRN, KC_RPRN, KC_UNDS,        _______, _______, _______ 
   ),
   [FUN ] = LAYOUT_miryoku(
-    KC_F12 , KC_F7  , KC_F8  , KC_F9  , KC_PSCR,        _______, _______, _______, _______, _______,      
+    KC_F12 , KC_F7  , KC_F8  , KC_F9  , KC_PSCR,        QK_BOOT, _______, _______, _______, _______,      
     KC_F11 , KC_F4  , KC_F5  , KC_F6  , KC_RALT,        _______, KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI,       
     KC_F10 , KC_F1  , KC_F2  , KC_F3  , KC_RALT,        _______, _______, _______, _______, _______,      
                       KC_ESC , KC_SPC , KC_TAB ,        _______, _______, _______ 
